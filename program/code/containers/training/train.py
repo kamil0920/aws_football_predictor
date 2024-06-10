a591a8779ae8b09855e10cebaf60a4906d1fa732
Kamil Mitura <kamilmituraa@gmail.com>
2024-06-10 21:52:27 +0200

    add comet experiments

--- program/code/containers/training/train.py
+++ program/code/containers/training/train.py
@@ -1,9 +1,11 @@
+import json
 import os
 import argparse
 import tarfile
 
 import numpy as np
 import pandas as pd
+from comet_ml import Experiment
 
 from pathlib import Path
 from sklearn.metrics import f1_score, log_loss
@@ -18,10 +20,10 @@ def evaluate_model(X_train, X_val, y_train, y_val, early_stopping_rounds, hyperp
     y_pred = model.predict(X_val)
     f1 = f1_score(y_val, y_pred)
 
-    return f1, model
+    return f1, model, y_pred
 
 
-def train(model_directory, train_path, validation_path, hyperparameters, pipeline_path, early_stopping_rounds=25):
+def train(model_directory, train_path, validation_path, hyperparameters, pipeline_path, experiment, early_stopping_rounds=25):
     train_files = [file for file in Path(train_path).glob("*.csv")]
     validation_files = [file for file in Path(validation_path).glob("*.csv")]
 
@@ -38,7 +40,7 @@ def train(model_directory, train_path, validation_path, hyperparameters, pipelin
     y_validation = X_validation[X_validation.columns[-1]]
     X_validation.drop(X_validation.columns[-1], axis=1, inplace=True)
 
-    f1, model = evaluate_model(X_train, X_validation, y_train, y_validation, early_stopping_rounds, hyperparameters)
+    f1, model, predictions = evaluate_model(X_train, X_validation, y_train, y_validation, early_stopping_rounds, hyperparameters)
 
     print("F1 score: {:.2f}".format(f1))
 
@@ -53,10 +55,26 @@ def train(model_directory, train_path, validation_path, hyperparameters, pipelin
     with tarfile.open(Path(pipeline_path) / "model.tar.gz", "r:gz") as tar:
         tar.extractall(model_directory)
 
+    if experiment:
+        experiment.log_parameters(
+            {
+                "eta": hyperparameters['eta'],
+                "max_depth": hyperparameters['max_depth'],
+                "subsample": hyperparameters['subsample'],
+                "colsample_bytree": hyperparameters['colsample_bytree'],
+                "min_child_weight": hyperparameters['min_child_weight'],
+                "reg_lambda": hyperparameters['reg_lambda'],
+                "reg_alpha": hyperparameters['reg_alpha'],
+            }
+        )
+        experiment.log_dataset_hash(X_train)
+        experiment.log_confusion_matrix(
+            y_validation.astype(int), predictions.astype(int)
+        )
+        experiment.log_model("penguins", model_filepath.as_posix())
+
 
 if __name__ == "__main__":
-    # Any hyperparameters provided by the training job are passed to
-    # the entry point as script arguments.
     parser = argparse.ArgumentParser()
     parser.add_argument("--early_stopping_rounds", type=int, default=25)
 
@@ -79,17 +97,35 @@ if __name__ == "__main__":
         "reg_lambda": args.lambda_,
         "reg_alpha": args.alpha,
     }
+
+    comet_api_key = os.environ.get("COMET_API_KEY", None)
+    comet_project_name = os.environ.get("COMET_PROJECT_NAME", None)
+
+    experiment = (
+        Experiment(
+            project_name=comet_project_name,
+            api_key=comet_api_key,
+            auto_metric_logging=True,
+            auto_param_logging=True,
+            log_code=True,
+        )
+        if comet_api_key and comet_project_name
+        else None
+    )
+
+    training_env = json.loads(os.environ.get("SM_TRAINING_ENV", {}))
+    job_name = training_env.get("job_name", None) if training_env else None
+
+    if job_name and experiment:
+        experiment.set_name(job_name)
+
     train(
-        # This is the location where we need to save our model. SageMaker will
-        # create a model.tar.gz file with anything inside this directory when
-        # the training script finishes.
         model_directory=os.environ["SM_MODEL_DIR"],
 
-        # SageMaker creates one channel for each one of the inputs to the
-        # Training Step.
         train_path=os.environ["SM_CHANNEL_TRAIN"],
         validation_path=os.environ["SM_CHANNEL_VALIDATION"],
         pipeline_path=os.environ["SM_CHANNEL_PIPELINE"],
+        experiment=experiment,
         early_stopping_rounds=args.early_stopping_rounds,
         hyperparameters=params
     )
